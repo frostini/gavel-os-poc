088319b93004208e4006f68b35e9c819efa9e4de
Marlon Frausto <marlonjfrausto@gmail.com>
2021-10-19 17:22:34 -0700

    updated apostrophy escaping to fix broken build

--- pages/auctions-show.js
+++ pages/auctions-show.js
@@ -25,7 +25,7 @@ const product = {
   href: '#',
   price: '$220',
   description:
-    "Don't compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
+    "Don&apos;t compromise on snack-carrying capacity with this lightweight and spacious bag. The drawstring top keeps all your favorite chips, crisps, fries, biscuits, crackers, and cookies secure.",
   imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg',
   imageAlt: 'Model wearing light green backpack with black canvas straps and front zipper pouch.',
   breadcrumbs: [
